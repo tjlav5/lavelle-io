c9f76c81486c6f6a2a99c744aa8f061a38b5ab89
TJ Lavelle <tj@lavelle.io>
2020-04-01 16:34:56 -0400

    Add routing PoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
 import * as React from "react";
 import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  RouteProps,
+  Redirect
+} from "react-router-dom";
 
 export const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div>
+        <button />
+
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/public">
+            <div />
+          </Route>
+          <Route path="/login">
+            <div />
+          </Route>
+          <PrivateRoute path="/protected">
+            <div />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
+  const isAuthenticated = false;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 };
