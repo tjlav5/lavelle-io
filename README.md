bb70493218a9dfe958ee081d174a54e255d94a5a
TJ Lavelle <tj@lavelle.io>
2020-04-01 00:02:31 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lavelle-io
-Created with CodeSandbox
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+};
