d40a104dd93661eabb08e13db1a41ad095fa1975
TJ Lavelle <tj@lavelle.io>
2020-04-13 00:44:09 -0400

    Return the UID for fetcher

--- functions/src/foobar.ts
+++ functions/src/foobar.ts
@@ -1,5 +1,8 @@
+import * as functions from "firebase-functions";
+
 import { FoobarRequest, FoobarResponse } from "./messages/foobar";
 
-export async function foobar(data: FoobarRequest, context: any) {
-  return { foo: `bar baz ${data.age}` } as FoobarResponse;
+export async function foobar(data: FoobarRequest, ctx: functions.EventContext) {
+  console.log(ctx);
+  return { foo: `bar baz ${data.age} ${ctx.auth?.uid}` } as FoobarResponse;
 }
