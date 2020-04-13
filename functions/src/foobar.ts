c496a8430cad6c4981c7f3f29335bb71d5e020b2
TJ Lavelle <tj@lavelle.io>
2020-04-13 00:47:15 -0400

    Fix CallableContext type

--- functions/src/foobar.ts
+++ functions/src/foobar.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 
 import { FoobarRequest, FoobarResponse } from "./messages/foobar";
 
-export async function foobar(data: FoobarRequest, ctx: functions.EventContext) {
+export async function foobar(data: FoobarRequest, ctx: functions.https.CallableContext) {
   console.log(ctx);
   return { foo: `bar baz ${data.age} ${ctx.auth?.uid}` } as FoobarResponse;
 }
